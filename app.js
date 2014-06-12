5e04647e91f86ffbf12a164c2db6ef2d69b3f8fc
menor <josemenor@gmail.com>
2014-06-12 11:36:41 +0200

    Add mock stories

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 
   app.controller( 'MainController', function( $scope ){
 
+    $scope.stories = [
+      { title:'Story 00', description: 'Description pending.'},
+      { title:'Story 01', description: 'Description pending.'},
+      { title:'Story 02', description: 'Description pending.'},
+      { title:'Story 03', description: 'Description pending.'},
+      { title:'Story 04', description: 'Description pending.'},
+      { title:'Story 05', description: 'Description pending.'}
+    ];
 
   });
 
