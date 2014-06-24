eca93f5fa823af51703198c14df4fb614e643b4d
menor <josemenor@gmail.com>
2014-06-24 07:42:13 +0200

    Add create story button

--- app.js
+++ app.js
@@ -9,6 +9,13 @@
       $scope.currentStory = story;
     };
 
+    $scope.createStory = function() {
+      $scope.stories.push({
+          title: 'New Story',
+          description: 'Description pending'
+      });
+    };
+
     $scope.stories = [
       { title:'Story 00', description: 'Description pending.'},
       { title:'Story 01', description: 'Description pending.'},
@@ -18,5 +25,5 @@
       { title:'Story 05', description: 'Description pending.'}
     ];
   });
-  
+
 })();
